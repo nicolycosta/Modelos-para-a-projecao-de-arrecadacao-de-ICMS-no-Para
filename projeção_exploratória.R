b13ac4a278f9ae9a44a18fff1d7dc6feb703dbc1
Nicoly Costa <54318133+nicolycosta@users.noreply.github.com>
2021-11-26 16:21:14 -0300

    Update projeção_exploratória.R

--- projeção_exploratória.R
+++ projeção_exploratória.R
@@ -1,6 +1,5 @@
-
 ################################################################################
-#Pacotes usados
+#Used packages
 install.packages("xlsx")
 install.packages('readxl')
 install.packages('forecast')
@@ -11,9 +10,7 @@ library(readxl)
 library(forecast)
 library(MLmetrics)
 library(formattable)
-
-
-
+library(ggplot2)
 
 ################################################################################
 #Downloading the main public revenue database
@@ -21,12 +18,12 @@ library(formattable)
 Base <- read_xlsx(path = "Base.xlsx", sheet = 1)
 Base$MES <- as.Date(Base$MES)
 Base <- data.frame(Base)
-head(Base, 6) #mostrando os 6 primeiros valores
-tail(Base) #mostrando os 6 últimos valores
+head(Base, 6) #showing the first 6 values
+tail(Base) #showing the last 6 values
 
 
 ################################################################################
-#TRANSFORMANDO AS VARIÁVEIS EM SÉRIES TEMPORAIS
+#transforming the variables into time series
 
 ICMS <- ts(Base$ICMS, start = c(2004,1), end = c(2021,10), frequency = 12)
 IPVA <- ts(Base$IPVA, start = c(2004,1), end = c(2021,10), frequency = 12)
@@ -34,25 +31,24 @@ ITCD <- ts(Base$ITCD, start = c(2004,1), end = c(2021,10), frequency = 12)
 
 
 ################################################################################
-#ANÁLISE EXPLORATÓRIA
-
+#exploratory analysis
 
 #ICMS
-plot(ICMS, xlab= 'Período', ylab= 'Arrecadação de ICMS')
+plot(ICMS, xlab= 'Periodo', ylab= 'Arrecadacao de ICMS')
 plot(decompose(ICMS, type= "multiplicative"), xlab= "Ano", col="black")
 ggseasonplot(ICMS)
 
-
 #IPVA
-plot(IPVA, xlab= 'Período', ylab= 'Arrecadação de IPVA')
+plot(IPVA, xlab= 'Periodo', ylab= 'Arrecadacao de IPVA')
 plot(decompose(IPVA, type= "multiplicative"), xlab= "Ano", col="black")
 ggseasonplot(IPVA)
 
-
 #ITCD
-plot(ITCD, xlab= 'Período', ylab= 'Arrecadação de ITCD')
+plot(ITCD, xlab= 'Periodo', ylab= 'Arrecadacao de ITCD')
 plot(decompose(ITCD, type= "multiplicative"), xlab= "Ano", col="black")
 ggseasonplot(ITCD, main="")
+
+#################################################################################
 
 #################################################################################
 
